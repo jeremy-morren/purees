76b797da42da8c0ec13f5a9a83a446f0f0429b28
Jeremy Morren <jeremy.morren@outlook.com>
2023-06-06 15:19:31 +0800

    Updated AggregateStore

--- Source/Core/PureES.Core/AggregateStore.cs
+++ Source/Core/PureES.Core/AggregateStore.cs
@@ -6,9 +6,9 @@ using PureES.Core.ExpBuilders.WhenHandlers;
 
 namespace PureES.Core;
 
-public class AggregateStore<TAggregate> : IAggregateStore<TAggregate> where TAggregate : notnull
+public sealed class AggregateStore<TAggregate> : IAggregateStore<TAggregate> where TAggregate : notnull
 {
-    protected readonly IEventStore EventStore;
+    private readonly IEventStore _eventStore;
     
     private readonly IServiceProvider _serviceProvider;
     private readonly AggregateFactory<TAggregate> _factory;
@@ -16,23 +16,24 @@ public class AggregateStore<TAggregate> : IAggregateStore<TAggregate> where TAgg
     public AggregateStore(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
-        EventStore = serviceProvider.GetRequiredService<IEventStore>();
+        
+        _eventStore = serviceProvider.GetRequiredService<IEventStore>();
         _factory = serviceProvider.GetRequiredService<PureESServices>()
                        .GetService<AggregateFactory<TAggregate>>()
                    ?? throw new InvalidOperationException($"Unable to get factory for aggregate {typeof(TAggregate)}");
     }
 
-    protected virtual IAsyncEnumerable<EventEnvelope> Load(string streamId,
+    private IAsyncEnumerable<EventEnvelope> Load(string streamId,
         CancellationToken cancellationToken) =>
-        EventStore.Read(streamId, cancellationToken);
+        _eventStore.Read(streamId, cancellationToken);
 
-    protected virtual IAsyncEnumerable<EventEnvelope> Load(string streamId,
+    private IAsyncEnumerable<EventEnvelope> Load(string streamId,
         ulong expectedRevision, CancellationToken cancellationToken) => 
-        EventStore.Read(streamId, expectedRevision, cancellationToken);
+        _eventStore.Read(streamId, expectedRevision, cancellationToken);
 
-    protected virtual IAsyncEnumerable<EventEnvelope> LoadPartial(string streamId,
+    private IAsyncEnumerable<EventEnvelope> LoadPartial(string streamId,
         ulong requiredRevision, CancellationToken cancellationToken) =>
-        EventStore.ReadPartial(streamId, requiredRevision, cancellationToken);
+        _eventStore.ReadPartial(streamId, requiredRevision, cancellationToken);
 
     ValueTask<TAggregate> IAggregateStore<TAggregate>.Create(IAsyncEnumerable<EventEnvelope> events, 
         CancellationToken cancellationToken) => _factory(events, _serviceProvider, cancellationToken);
