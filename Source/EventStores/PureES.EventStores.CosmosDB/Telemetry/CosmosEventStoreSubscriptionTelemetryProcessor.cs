37a8975b088d7cd6e18365a4e66a06ffd512195b
Jeremy Morren <jeremy.morren@outlook.com>
2023-12-05 15:53:16 +0800

    Fixed cosmos telemetry

---
+++ Source/EventStores/PureES.EventStores.CosmosDB/Telemetry/CosmosEventStoreSubscriptionTelemetryProcessor.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace PureES.EventStores.CosmosDB.Telemetry;
+
+/// <summary>
+/// <see cref="CosmosEventStoreSubscriptionTelemetryInitializer"/>
+/// </summary>
+[UsedImplicitly]
+internal class CosmosEventStoreSubscriptionTelemetryProcessor : ITelemetryProcessor
+{
+    private readonly ITelemetryProcessor _next;
+
+    public CosmosEventStoreSubscriptionTelemetryProcessor(ITelemetryProcessor next)
+    {
+        _next = next;
+    }
+
+    public void Process(ITelemetry item)
+    {
+        if (item.Context.Operation.SyntheticSource == CosmosEventStoreSubscriptionTelemetryInitializer.SyntheticSource)
+            return;
+        _next.Process(item);
+    }
+}
