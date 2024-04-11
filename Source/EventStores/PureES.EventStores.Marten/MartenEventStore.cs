93502616f0157927a44c4675bba659dc538d7a68
Jeremy Morren <jeremy.morren@outlook.com>
2024-04-11 18:16:01 +0800

    Removed usings

--- Source/EventStores/PureES.EventStores.Marten/MartenEventStore.cs
+++ Source/EventStores/PureES.EventStores.Marten/MartenEventStore.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections.Immutable;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using Marten;
 using Marten.Exceptions;
-using Npgsql;
 
 namespace PureES.EventStores.Marten;
 
